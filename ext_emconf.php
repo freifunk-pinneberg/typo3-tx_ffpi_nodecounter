26a655c4825a43cbe4bf889c623c8e76954fcc37
Kevin Quiatkowski <kontakt@kevinquiatkowski.de>
2016-07-24 20:48:24 +0200

    Make it beta

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,16 +12,16 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Nodecounter',
-	'description' => '',
+	'description' => 'Counts Freifunk nodes',
 	'category' => 'plugin',
 	'author' => 'Kevin Quiatkowski',
 	'author_email' => 'kevin@pinneberg.freifunk.net',
-	'state' => 'alpha',
+	'state' => 'beta',
 	'internal' => '',
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '',
+	'version' => '2.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '7.6.0-7.6.99',
