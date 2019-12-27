cdca40e2bff0eb398876033f09bc7ce36c2e8b61
Kevin Quiatkowski <kontakt@kevinquiatkowski.de>
2019-12-27 12:09:07 +0100

    Cleanup deprecations

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,2 @@
+<?php
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('ffpi_nodecounter', 'Configuration/TypoScript', 'Nodecounter');
---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,7 @@
+<?php
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'FFPI.' . 'ffpi_nodecounter',
+    'Counter',
+    'counter'
+);
