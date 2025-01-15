50520b40b007a4366f68ccc7683b8d7b86a6ad5e
Kevin Quiatkowski <kontakt@kevinquiatkowski.de>
2025-01-15 12:40:27 +0100

    Fix version number

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.2.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.0.0-11.4.99',
+            'typo3' => '10.0.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
